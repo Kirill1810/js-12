6152816417be2d15cf1d5101637c0de739d78761
Kirill1810 <129986639+Kirill1810@users.noreply.github.com>
2024-12-25 18:52:27 +0300

    пр

--- sj.js
+++ sj.js
@@ -1,154 +1,145 @@
 // //1
 
-// const user = {
-//     name: "John",
-//     age: 30,
-//     hobby: "reading",
-//     premium: true
-//   };
+const user = {
+    name: "John",
+    age: 30,
+    hobby: "reading",
+    premium: true
+  };
   
 
-//   user.mood = 'happy';
+  user.mood = 'happy';
   
 
-//   user.hobby = 'skydiving';
+  user.hobby = 'skydiving';
   
 
-//   user.premium = false;
+  user.premium = false;
   
 
-//   for (let key of Object.keys(user)) {
+  for (let key of Object.keys(user)) {
 
-//     console.log(`${key}: ${user[key]}`);
+    console.log(`${key}: ${user[key]}`);
 
-//   }
+  }
 
 
 
 //   //2
 
-//   function countProps(obj) {
-//     return Object.keys(obj).length;
-//   }
+  function countProps(obj) {
+    return Object.keys(obj).length;
+  }
 
 
-//   const user = {
-//     name: "kirill",
-//     age: 15,
+  const user = {
+    name: "kirill",
+    age: 15,
   
  
-//   };
+  };
   
-//   console.log(countProps(user));
+  console.log(countProps(user));
 
   //3
 
-  // const employeesBase = {
-  //   kirill: 8,
-  //   sasha: 4000,
-  //   ura: 90,
-  //   ivan: 100,
-  // };
+  const employeesBase = {
+    kirill: 8,
+    sasha: 4000,
+    ura: 90,
+    ivan: 100,
+  };
   
-  // function findBestEmployee(employees) {
+  function findBestEmployee(employees) {
 
-  //   let maxEmployes = 0;
+    let maxEmployes = 0;
 
-  //   let nameBest = "";
+    let nameBest = "";
 
-  //   for (const ents of Object.entries(employees)) {
+    for (const ents of Object.entries(employees)) {
 
-  //     let names = ents[0];
+      let names = ents[0];
 
-  //     let workes = ents[1];
+      let workes = ents[1];
 
-  //     if (workes > maxEmployes) {
+      if (workes > maxEmployes) {
 
-  //       maxEmployes = workes;
+        maxEmployes = workes;
 
-  //       nameBest = names;
-  //     }
-  //   }
+        nameBest = names;
+      }
+    }
     
-  //   return nameBest;
-  // }
+    return nameBest;
+  }
   
-  // console.log(findBestEmployee(employeesBase));
+  console.log(findBestEmployee(employeesBase));
 
 
   //4
 
 
-  // const salary = {
-  //   Oleg: 200,
-  //   Karina: 500,
-  //   Arina: 700,
-  // };
+  const salary = {
+    Oleg: 200,
+    Karina: 500,
+    Arina: 700,
+  };
   
-  // function countTotalSalary(employes) {
-  //   let sumSalary = 0;
-  //   const valueArray = Object.values(employes);
-  //   for (let i = 0; i < valueArray.length; i++) {
-  //     sumSalary += valueArray[i];
-  //   }
-  //   for (const arr of Object.entries(salary)) {
-  //     const keys = arr[0];
-  //     const valueses = arr[1];
-  //     console.log(`${keys}: ${valueses}`);
-  //   }
-  //   return ` сума зарплати  становить ${sumSalary} грн`;
-  // }
-  // console.log(countTotalSalary(salary));
+  function countTotalSalary(employes) {
+    let sumSalary = 0;
+    const valueArray = Object.values(employes);
+    for (let i = 0; i < valueArray.length; i++) {
+      sumSalary += valueArray[i];
+    }
+    for (const arr of Object.entries(salary)) {
+      const keys = arr[0];
+      const valueses = arr[1];
+      console.log(`${keys}: ${valueses}`);
+    }
+    return ` сума зарплати  становить ${sumSalary} грн`;
+  }
+  console.log(countTotalSalary(salary));
   
 
   //5
 
   function getAllPropValues(arr, prop) {
-    return arr.map(item => item[prop]);
-}
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      result.push(arr[i][prop]);
+    }
+    return result;
+  }
 
 
-const objects = [
-  { name: 'Alice', age: 25 },
-  { name: 'Bob', age: 30 },
-  { name: 'Charlie', age: 35 }
-];
-
-const names = getAllPropValues(objects, 'name');
-console.log(names);  
-
-const ages = getAllPropValues(objects, 'age');
-console.log(ages); 
-
+  const users = [
+    { name: 'Аня', age: 15 },
+    { name: 'Ваня', age: 300 },
+    { name: 'Саня', age: 50 }
+  ];
+  
+  console.log(getAllPropValues(users, 'name')); 
+  console.log(getAllPropValues(users, 'age'));  
   //6
 
   function calculateTotalPrice(allProducts, productName) {
-    
- 
-    const product = allProducts.find(item => item.name === productName);
-    
-    
-    if (product) {
-        return product.price * product.quantity;
+    let totalPrice = 0;
+    for (let i = 0; i < allProducts.length; i++) {
+      if (allProducts[i].name === productName) {
+        totalPrice += allProducts[i].price * allProducts[i].quantity;
+      }
     }
-    
-    
-    return 0;
-}
+    return totalPrice;
+  }
+
+  const products = [
+    { name: 'диня', price: 2, quantity: 1 },
+    { name: 'яблуко', price: 5, quantity: 8 },
+    { name: 'кавун', price: 3, quantity: 10 },
+   
+  ];
   
-  
 
-const products = [
-  { name: 'Apple', price: 2, quantity: 5 },
-  { name: 'Banana', price: 1, quantity: 10 },
-  { name: 'Orange', price: 3, quantity: 7 }
-];
-
-const totalPrice = calculateTotalPrice(products, 'Banana');
-console.log(totalPrice);  
-
-const totalPrice2 = calculateTotalPrice(products, 'Apple');
-console.log(totalPrice2);  
-
-const totalPrice3 = calculateTotalPrice(products, 'Mango');
-console.log(totalPrice3); 
+  console.log(calculateTotalPrice(products, 'Apple'));  
+  console.log(calculateTotalPrice(products, 'Banana')); 
+  console.log(calculateTotalPrice(products, 'Grapes')); 
